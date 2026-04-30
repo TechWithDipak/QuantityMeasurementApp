5029202527bb7ea41c77ddc807e6b741f2266f69
Dipak kumar <dipak.yadav04112@gmail.com>
2026-04-30 15:36:52 +0530

    Implement length measurement functionality
    Added LengthUnit enum and QuantityLength class for length measurement.

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,2 +1,45 @@
+package com.quantit;
+
 public class QuantityMeasurementApp {
+
+    public enum LengthUnit {
+        FEET(1.0),
+        INCH(1.0/12.0);
+
+        private final double conversionFactor;
+
+        LengthUnit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
+        }
+
+        public double toBaseUnit(double value) {
+            return value * conversionFactor;
+        }
+    }
+
+    public static class QuantityLength {
+        private final double value;
+        private final LengthUnit unit;
+
+        public QuantityLength(double value, LengthUnit unit) {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            QuantityLength other = (QuantityLength) obj;
+            double thisBase = this.unit.toBaseUnit(this.value);
+            double otherBase = other.unit.toBaseUnit(other.value);
+            return Double.compare(thisBase, otherBase) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+        System.out.println(q1.equals(q2));
+    }
 }
